40e99b74f52b034d8780e679b46c808dd0842e29
Rafael Afonso <rafael.afonsof.lima@gmail.com>
2024-07-22 13:53:36 -0300

    Commit

--- landing-page/src/app/layout.jsx
+++ landing-page/src/app/layout.jsx
@@ -1,7 +1,13 @@
 import { Inter } from "next/font/google";
 import "../styles/globals.scss";
+import { Roboto } from 'next/font/google'
 
 const inter = Inter({ subsets: ["latin"] });
+ 
+const roboto = Roboto({
+  weight: ['100', '300', '400', '500', '700'],
+  subsets: ['latin'],
+})
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto.className}>{children}</body>
     </html>
   );
 }
--- landing-page/src/components/features/index.jsx
+++ landing-page/src/components/features/index.jsx
@@ -12,35 +12,31 @@ export const Features = () => {
 
         <div className={Styles.features}>
             <div className={Styles.feature}>
-                <Image src={Icon} alt='icon' className='icon'/>
+                <Image src={Icon} alt='icon' className='icon' />
                 <h1>Digital Strategy</h1>
                 <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt eos non rerum? Optio
-                    nesciunt nemo beatae placeat repudiandae quod harum nam, eos illo temporibus, doloremque, autem iure totam numquam voluptate.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.
                 </p>
             </div>
             <div className={Styles.feature}>
-                <Image src={Icon} alt='icon' className='icon'/>
+                <Image src={Icon} alt='icon' className='icon' />
                 <h1>Estratégia digital</h1>
                 <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt eos non rerum? Optio
-                    nesciunt nemo beatae placeat repudiandae quod harum nam, eos illo temporibus, doloremque, autem iure totam numquam voluptate.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.
                 </p>
             </div>
             <div className={Styles.feature}>
-                <Image src={Icon} alt='icon' className='icon'/>
+                <Image src={Icon} alt='icon' className='icon' />
                 <h1>Social Midia</h1>
                 <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt eos non rerum? Optio
-                    nesciunt nemo beatae placeat repudiandae quod harum nam, eos illo temporibus, doloremque, autem iure totam numquam voluptate.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.
                 </p>
             </div>
             <div className={Styles.feature}>
-                <Image src={Icon} alt='icon' className='icon'/>
+                <Image src={Icon} alt='icon' className='icon' />
                 <h1>Social Midia</h1>
                 <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt eos non rerum? Optio
-                    nesciunt nemo beatae placeat repudiandae quod harum nam, eos illo temporibus, doloremque, autem iure totam numquam voluptate.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.
                 </p>
             </div>
         </div>
